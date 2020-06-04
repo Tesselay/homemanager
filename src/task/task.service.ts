7968e120fb27e8bb55bef2ae311a27d487daca1d
Dominique Lahl <domee.lahl@gmail.com>
2020-06-04 22:26:49 +0200

    Add basic functionality to task service

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -9,4 +9,31 @@ import { CreateTaskDTO } from "./create-task.dto";
 @Injectable()
 export class TaskService {
 
+    constructor(@InjectModel("Task") private readonly taskModel: Model<Task>) {} 
+
+    async getTasks(): Promise<Task[]> {
+        const tasks = await this.taskModel.find().exec();
+        return tasks;
+    }
+
+    async getTask(taskId): Promise<Task> {
+        const task = await this.taskModel.findById(taskId).exec();
+        return task;
+    }
+    
+    async addTask(createTaskDTO: CreateTaskDTO): Promise<Task> {
+        const newTask = await this.taskModel(createTaskDTO);
+        return newTask.save();
+    }
+
+    async editTask(taskId, createTaskDTO: CreateTaskDTO): Promise<Task> {
+        const updatedTask = await this.taskModel.findByIdAndUpdate(taskId, createTaskDTO, { new: true });
+        return updatedTask;
+    }
+
+    async deleteTask(taskId): Promise<any> {
+        const deletedTask = await this.taskModel.findByIdAndRemove(taskId);
+        return deletedTask;
+    }
+    
 }
