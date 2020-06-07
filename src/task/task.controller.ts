177969e9dd93c789f4264920f6b4392086084701
Dominique Lahl <domee.lahl@gmail.com>
2020-06-07 16:12:22 +0200

    Implement controller; Implement validation; Add CORS

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, HttpStatus, Param, NotFoundException, Post, Body, Query, Put, Delete } from '@nestjs/common';
+import { TaskService } from "./task.service";
+import { CreateTaskDTO } from "./create-task.dto";
+import { ValidateObjectId } from "../shared/pipes/validate-object-id.pipes";
+import { getHeapStatistics } from 'v8';
 
-@Controller('task')
-export class TaskController {}
+
+@Controller("tasks")
+export class TaskController {
+
+    constructor(private taskService: TaskService) {}
+
+    @Get("task")
+    async getTasks(@Res() res) {
+        const tasks = await this.taskService.getTasks();
+        return res.status(HttpStatus.OK).json(tasks);
+    }
+
+    @Get("task/:taskID")
+    async getTask(@Res() res, @Param("taskID", new ValidateObjectId()) taskID) {
+        const task = await this.taskService.getTask(taskID);
+        if (!task) throw new NotFoundException("Task does not exist!");
+        return res.status(HttpStatus.OK).json(task);
+    }
+
+    @Post("/post")
+    async addTask(@Res() res, @Body() createTaskDTO: CreateTaskDTO) {
+        const newTask = await this.taskService.addTask(createTaskDTO);
+        return res.status(HttpStatus.OK).json({
+            message: "Task has been submitted successfully!",
+            task: newTask
+        })
+    }
+
+    @Put('/edit')
+    async editTask(
+        @Res() res,
+        @Query('taskID', new ValidateObjectId()) taskID,
+        @Body() createTaskDTO: CreateTaskDTO
+    ) {
+        const editedTask = await this.taskService.editTask(taskID, createTaskDTO);
+        if (!editedTask) throw new NotFoundException('Task does not exist!');
+        return res.status(HttpStatus.OK).json({
+            message: 'Task has been successfully updated',
+            task: editedTask
+        })
+    }
+
+    @Delete('/delete')
+    async deletePost(@Res() res, @Query('taskID', new ValidateObjectId()) taskID) {
+        const deletedTask = await this.taskService.deleteTask(taskID);
+        if (!deletedTask) throw new NotFoundException('Task does not exist!');
+        return res.status(HttpStatus.OK).json({
+            message: 'Task has been deleted!',
+            task: deletedTask
+        })
+    }
+
+}
--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,7 +3,13 @@ import { Module } from '@nestjs/common';
 import { TaskService } from "./task.service";
 import { TaskController } from "./task.controller";
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskSchema } from './task.schema';
+
 @Module({
+    imports: [
+        MongooseModule.forFeature([{ name: 'Task', schema: TaskSchema }])
+    ],
     controllers: [TaskController],
     providers: [TaskService],
 })
